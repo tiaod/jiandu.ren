94c140beb1f91f20e35d93df2dd17e20f29eaf49
tiaod <tiaod@qq.com>
2019-06-13 21:12:09 +0800

    update test

--- server/test/rss.js
+++ server/test/rss.js
@@ -10,6 +10,22 @@ test.before(async t=>{
   await broker.start()
 })
 
+test.only('测试fetch',async t=>{
+  let stream = await feedService.fetch(`https://www.feedforall.com/sample.xml?t=${Date.now()}`)
+  console.log(stream)
+  stream.on('readable', function () {
+    // This is where the action is!
+    var stream = this; // `this` is `feedparser`, which is a stream
+    var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+    var item;
+
+    while (item = stream.read()) {
+      console.log(item)
+    }
+  })
+  t.pass()
+})
+
 test('创建feed', async t=> {
   let feed = await managerService.actions.createFeed({
     url: `https://www.feedforall.com/sample.xml?t=${Date.now()}`
